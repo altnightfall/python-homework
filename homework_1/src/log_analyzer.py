24640c8d0363fc1d3f3dd3899193cbb754f94230
Altnightfall <altnightfall@yandex.ru>
2025-04-09 21:33:51 +0300

    added pylint to pre-hook

--- homework_1/src/log_analyzer.py
+++ homework_1/src/log_analyzer.py
@@ -1,3 +1,5 @@
+"""Module for log analyzer"""
+
 # log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
 #                     '$status $body_bytes_sent "$http_referer" '
 #                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
@@ -7,8 +9,8 @@ config = {"REPORT_SIZE": 1000, "REPORT_DIR": "./reports", "LOG_DIR": "./log"}
 
 
 def main():
-    #test_precommit_on_new_machine
-    pass
+    """Main function to execute"""
+    return
 
 
 if __name__ == "__main__":
